590f656f5e33037498d21c2a8e7750596149d830
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2025-01-16 09:46:15 +0100

    first commit

---
+++ launch/ekf_launch.py
@@ -0,0 +1,21 @@
+from simple_launch import SimpleLauncher
+
+
+def generate_launch_description():
+
+    sl = SimpleLauncher(use_sim_time=True)
+    sl.declare_arg('rviz', True)
+    sl.declare_arg('manual', True)
+
+    with sl.group(if_arg = 'rviz'):
+        sl.rviz(sl.find('ecn_aquabot', 'config.rviz'))
+
+    # TODO run gps2pose from aquabot_ekf pkg
+    # and robot_localization with config file aquabot_ekf/ekf.yaml
+
+
+    with sl.group(if_arg='manual'):
+        sl.node('slider_publisher',
+                arguments = [sl.find('ok_aquabot', 'props.yaml')])
+
+    return sl.launch_description()
