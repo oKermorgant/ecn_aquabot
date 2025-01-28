d1f48c7106f88cc435bd066799547ee054ae135f
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2025-01-28 12:13:05 +0100

    ecn_aquabot

--- launch/ekf_launch.py
+++ launch/ekf_launch.py
@@ -13,9 +13,8 @@ def generate_launch_description():
     # TODO run gps2pose from aquabot_ekf pkg
     # and robot_localization with config file aquabot_ekf/ekf.yaml
 
-
     with sl.group(if_arg='manual'):
         sl.node('slider_publisher',
-                arguments = [sl.find('ok_aquabot', 'props.yaml')])
+                arguments = [sl.find('ecn_aquabot', 'props.yaml')])
 
     return sl.launch_description()
